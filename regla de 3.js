ec3f4120d74f162a8e59c6079ad7267e64cee0e4
Adrián Torres <binarytreecode@gmail.com>
2021-10-26 08:24:48 -0500

    hice html y lo traje en objeto al js

---
+++ regla de 3.js
@@ -0,0 +1,12 @@
+var txt1 = document.getElementById("magnitud 1");
+var txt2 = document.getElementById("magnitud 2");
+var txt3 = document.getElementById("magnitud 3");
+
+var dato1 = document.getElementById("magnitud1_dato1");
+var dato2 = document.getElementById("magnitud2_dato1");
+var dato3 = document.getElementById("magnitud3_dato1");
+var dato4 = document.getElementById("magnitud1_dato2");
+var Dato5 = document.getElementById("magnitud2_dato2");
+var dato6 = document.getElementById("magnitud3_dato2");
+
+
