e91578803e75cdd60c2a43da7a03b985147e2c58
Adrián Torres <binarytreecode@gmail.com>
2021-10-26 14:34:49 -0500

    añadi opicones de check-box al html mejore form_nombres y traje datos al js

--- regla de 3.js
+++ regla de 3.js
@@ -6,7 +6,27 @@ var dato1 = document.getElementById("magnitud1_dato1");
 var dato2 = document.getElementById("magnitud2_dato1");
 var dato3 = document.getElementById("magnitud3_dato1");
 var dato4 = document.getElementById("magnitud1_dato2");
-var Dato5 = document.getElementById("magnitud2_dato2");
+var dato5 = document.getElementById("magnitud2_dato2");
 var dato6 = document.getElementById("magnitud3_dato2");
 
+var txt1Value, txt2Value, txt3Value;
 
+
+
+function sumit () {
+    txt1Value = txt1.value;
+    txt2Value = txt2.value;
+    txt3Value = txt3.value;    
+
+    mag1_date1 = parseInt(dato1.value);
+    mag2_date1 = parseInt(dato2.value);
+    mag3_date1 = parseInt(dato3.value);
+
+    mag1_date2 = parseInt(dato4.value);
+    mag2_date2 = parseInt(dato5.value);
+    mag3_date2 = parseInt(dato6.value);
+}
+
+function operaciones() {
+    
+}
